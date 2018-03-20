e630498fe2aebffb97a9c6cc7b54eebc0ce84ed3
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-03-20 22:33:45 +0530

    add syscall

---
+++ rtnice.c
@@ -0,0 +1,25 @@
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/sched.h>
+#include<linux/syscalls.h>
+#include "rtnice.h"
+
+asmlinkage long sys_rtnice(long givenPID, int rtnice) {
+    struct task_struct *proces;
+    int flag = 0;
+
+    for_each_process(proces) {
+      if((long)task_pid_nr(proces)==givenPID)
+      {
+        flag = 1;
+        proces->se.rtnice = rtnice;
+      }
+  }
+
+  if(flag==0)
+  {
+    printk("No process with the given PID found\n");
+  }
+
+  return 0;
+}
