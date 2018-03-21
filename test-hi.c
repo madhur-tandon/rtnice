0d5106fa2d22259a2622c6796fefd1451c8247ac
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-03-21 16:45:14 +0530

    hi

---
+++ test-hi.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+
+int main()
+{
+  struct timeval start;
+  gettimeofday(&start,NULL);
+  long int pid=fork();
+
+  printf("System call sys_rt_nice boo %ld\n",pid);
+
+  if(pid!=0)
+      {
+          printf("System call sys_rt_nice returned %ld\n",pid);
+          printf("System call sys_rt_nice not returned %ld\n",getpid());
+          syscall(323,pid,200);
+      }
+
+  if(pid==0)
+      {
+          struct timeval stop;
+          long prod=1;
+          long i=1;
+          for(i=1;i<10000000000;i++)
+          {
+              prod=prod*i;
+              //if(i%1000000==0)   
+                  //syscall(324,getpid());       
+          }
+          gettimeofday(&stop,NULL);
+          //clock_t end = clock();
+          printf("Time %lu PID %ld \n",stop.tv_usec - start.tv_usec,getpid());
+         
+      }
+
+  else
+      {
+          struct timeval stop;
+         
+          long prod=1;
+          long i=1;
+          for(i=1;i<100000000;i++)
+          {
+              prod=prod*i;
+              //printf("%ld\n",getpid());
+              //syscall(324,getpid());
+             
+             
+          }
+          gettimeofday(&stop,NULL);
+         
+          //clock_t end = clock();   
+          printf("Time %lu PID %ld \n",stop.tv_usec - start.tv_usec,getpid());
+          wait(pid);
+      }
+  return 0;
+}
