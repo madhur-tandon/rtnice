7e2d2c27f8a7e7a6403d86fb16160fd917811121
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-03-21 16:50:10 +0530

    hi2

--- test.c
+++ test.c
@@ -5,68 +5,63 @@
 #include <time.h>
 #include <sys/time.h>
 
-long long nCr(long long n, long long r)
-{
-  if(r!=0 && n!=r)
-  {
-    return nCr(n-1,r-1) + nCr(n-1,r);
-  }
-  else
-  {
-    return 1;
-  }
-}
-
 int main()
 {
-    struct timeval start;
-    gettimeofday(&start,NULL);
-    long int pid=fork();
-   
-    printf("System call sys_rt_nice boo %ld\n",pid);
-   
-    if(pid!=0)
-    {
-        printf("System call sys_rt_nice returned %ld\n",pid);
-        printf("System call sys_rt_nice not returned %ld\n",getpid());
-        syscall(323,pid,200);
-    }
+	
+	//printf("System call sys_rt_nice returned %ld\n",amma);
+	struct timeval start;
+	//clock_t begin =clock();
+	gettimeofday(&start,NULL);
+	long int pid=fork();
+	
+	
+	//unsigned long long int n
+	printf("System call sys_rt_nice boo %ld\n",pid);
+	
+	if(pid!=0)
+	{
+		printf("System call sys_rt_nice returned %ld\n",pid);
+		printf("System call sys_rt_nice not returned %ld\n",getpid());
+		//syscall(323,pid,200);
+	}
 
-    if(pid==0)
-    {
-        struct timeval stop;
-        long prod=1;
-        long i=1;
-        for(i=1;i<10000000000;i++)
-        {
-            prod=prod*i;
-            //if(i%1000000==0)   
-                //syscall(324,getpid());       
-        }
-        gettimeofday(&stop,NULL);
-        //clock_t end = clock();
-        printf("Time %lu PID %ld \n",stop.tv_usec - start.tv_usec,getpid());
-       
-    }
-    else
-    {
-        struct timeval stop;
-       
-        long prod=1;
-        long i=1;
-        for(i=1;i<100000000;i++)
-        {
-            prod=prod*i;
-            //printf("%ld\n",getpid());
-            //syscall(324,getpid());
-           
-           
-        }
-        gettimeofday(&stop,NULL);
-       
-        //clock_t end = clock();   
-        printf("Time %lu PID %ld \n",stop.tv_usec - start.tv_usec,getpid());
-        wait(pid);
-    }
-    return 0;
+	
+	if(pid==0)
+	{
+		struct timeval stop;
+		//clock_t begin =clock();
+		long prod=1;
+		long i=1;
+		for(i=1;i<1000000000;i++)
+		{
+			prod=prod*i;
+			//if(i%1000000==0)	
+				//syscall(324,getpid());		
+		}
+		gettimeofday(&stop,NULL);
+		//clock_t end = clock();
+		printf("Time %lu PID %ld \n",stop.tv_usec - start.tv_usec,getpid());
+		
+	}
+	else
+	{
+		struct timeval stop;
+		
+		long prod=1;
+		long i=1;
+		for(i=1;i<1000000000;i++)
+		{
+			prod=prod*i;
+			//printf("%ld\n",getpid());
+			//syscall(324,getpid());
+			
+			
+		}
+		gettimeofday(&stop,NULL);
+		
+		//clock_t end = clock();	
+		printf("Time %lu PID %ld \n",stop.tv_usec - start.tv_usec,getpid());
+		wait(pid);
+	}
+	return 0;
 }
